21047fd617194648723ebceaaf74991a40b5207e
RuiJ72 <rlagos900@gmail.com>
2022-07-07 01:43:58 -0300

    Ejercício 1

---
+++ VariablesEjercicio_1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("_____________Welcome_____________");
+
+
+Console.WriteLine(" Please, introduce your full name: ");
+string name = Console.ReadLine();
+
+
+Console.WriteLine(" What is your age?: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Inform your Birthday year: ");
+int dateBirth = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" Do you know any programming language? Signal with Y or N ");
+char programing = Convert.ToChar(Console.ReadLine());
+
+
+Console.WriteLine(" Your name is: " + name + 
+    ", you have " + age + " years old, " + "and you born in the year of " + dateBirth + 
+    ". You answered with " + programing + " about your programming skills ");
+    
