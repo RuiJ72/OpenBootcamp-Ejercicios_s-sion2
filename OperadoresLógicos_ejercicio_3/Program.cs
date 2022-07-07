f59e679a5387c7b77bd7b9eca440f4b7a597b0eb
RuiJ72 <rlagos900@gmail.com>
2022-07-07 19:56:08 -0300

    Ejercicio2/No terminado

--- OperadoresLógicos_ejercicio_3/Program.cs
+++ OperadoresLógicos_ejercicio_3/Program.cs
@@ -1,13 +1,13 @@
-﻿//int number = 18;
+﻿int number = 18;
 //char var = 'a' = 65;
 
 
-//Console.WriteLine("Condition True");
-//Console.WriteLine("---------------------------");
-//Console.WriteLine(18 == 18 && 17 <= 18);
+Console.WriteLine("Condition True");
+Console.WriteLine("---------------------------");
+Console.WriteLine(18 == 18 && 17 <= 18);
 
 //Console.WriteLine("----------------------------");
 //Console.WriteLine("Condition True and False");
 //Console.WriteLine("----------------------------");
 
-//Console.WriteLine( 19 > 18 || 65 = 65);
+//Console.WriteLine(19 > 18 || 65 = 65);
